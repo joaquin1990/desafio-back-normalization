fffaa57fa8aa9556b912939b488f6d345cc2f66e
joaquin1990 <joaquingarese@gmail.com>
2022-09-08 16:15:18 -0300

    AnotherOne

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ import { Server } from "socket.io";
 import Contenedor from "./managers/product.manager.js";
 import UserManager from "./managers/user.manager.js";
 import db from "./db/sqlBase.js";
+// Mongoose
+import mongoose from "mongoose";
+import chatsService from "./models/Chats.js";
+import { normalize, schema } from "normalizr";
 
 // const PORT = process.env.PORT || 8080;
 const PORT = 8080;
@@ -15,6 +19,7 @@ const io = new Server(server); //nuestro socket deberia estar corrindo
 const container = new Contenedor();
 const userManager = new UserManager();
 
+app.use(express.json());
 app.engine("handlebars", handlebars.engine());
 app.set("views", __dirname + "/views");
 app.set("view engine", "handlebars");
@@ -22,21 +27,58 @@ app.use(express.static(__dirname + "/public"));
 
 app.use("/", viewsRouter);
 
+// Schema and chat normalization:
+const author = new schema.Entity("authors", {}, { idAttribute: "email" });
+const message = new schema.Entity(
+  "messages",
+  {
+    author: author,
+  },
+  { idAttribute: "_id" }
+);
+const chat = new schema.Entity("chats", {
+  chats: [message],
+});
+
+// Mongoose connection:
+const connection = mongoose.connect(
+  "mongodb+srv://joaquingarese:1a2b3c@cluster0.dcv0epl.mongodb.net/?retryWrites=true&w=majority",
+  (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Atlas DB connected");
+    }
+  }
+);
+
 io.on("connection", async (socket) => {
   console.log("socket connected");
   let products = await db("products").select("*");
   let chatLog = await db("chat").select("*");
   io.emit("updateProductList", products);
-  io.emit("log", chatLog);
+  // io.emit("log", chatLog);
+  io.emit(
+    "log",
+    normalize({ id: 1, chats: await chatsService.find({}) }, chat)
+  );
 
   //   Chat
-  socket.on("message", async (data) => {
-    let date = new Date(Date.now()).toLocaleString();
-    data.date = date;
-    await db("chat").insert(data);
-    let chatLog = await db("chat").select("*");
-    io.emit("log", chatLog);
-  });
+  socket.on(
+    "message",
+    // async (data) => {
+    //   let date = new Date(Date.now()).toLocaleString();
+    //   data.date = date;
+    //   await db("chat").insert(data);
+    //   let chatLog = await db("chat").select("*");
+    async (data) => {
+      await chatsService.create(data);
+      io.emit(
+        "log",
+        normalize({ id: 1, chats: await chatsService.find({}) }, chat)
+      );
+    }
+  );
 
   //   Show Products
   socket.on("addNewProduct", async (newProduct) => {
